4b73ee064db91c6e4d65d8beb5aac8e49dfd3118
zhu-hong <zhxhyy@qq.com>
2021-10-26 21:34:48 +0800

    disorder

--- src/routers/index.js
+++ src/routers/index.js
@@ -5,13 +5,23 @@ const router = createRouter({
     {
       path: '/',
       component: () => import('../views/index.vue'),
+      meta: {
+        title: 'Vue-Plugin',
+      },
     },
     {
       path: '/lazyload',
       component: () => import('../views/Lazyload/index.vue'),
+      meta: {
+        title: 'Lazyload',
+      },
     },
   ],
   history: createWebHistory(),
+})
+
+router.beforeEach((to) => {
+  document.title = to.meta?.title
 })
 
 export default router
