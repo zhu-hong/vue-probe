fae02cf2b258542e8951722638e3b5f4047e90ad
zhu-hong <zhxhyy@qq.com>
2021-10-28 12:30:38 +0800

    plugin VirtualScroll finally

--- plugin/VirtualScroll/index.vue
+++ plugin/VirtualScroll/index.vue
@@ -1,9 +1,59 @@
 <script>
+import { computed, ref } from '@vue/reactivity'
+import { nextTick } from '@vue/runtime-core'
+import { throttle } from '../../shared'
+
 export default {
-  name: 'VirtualScroll',
+  name: 'virtual-scroll',
+}
+</script>
+
+<script setup>
+const props = defineProps({
+  totalData: {
+    type: Array,
+    default: [],
+  },
+  itemHeight: {
+    type: Number,
+  }
+})
+
+const container = ref(null)
+const containSize = ref(0)
+const startIndex = ref(0)
+const endIndex = computed(() => startIndex.value + containSize.value > props.totalData.length - 1 ? props.totalData.length - 1 : startIndex.value + containSize.value)
+const renderData = computed(() => props.totalData.slice(startIndex.value, endIndex.value))
+const fillPadding = computed(() => {
+  return {
+    padding: `${startIndex.value * props.itemHeight}px 0 ${(props.totalData.length - 1 - endIndex.value) * props.itemHeight}px 0`
+  }
+})
+
+nextTick(() => {
+  containSize.value = Math.floor(container.value.offsetHeight / props.itemHeight) + 2
+})
+
+const handleScroll = (e) => {
+  startIndex.value = Math.floor(e.target.scrollTop / props.itemHeight)
 }
 </script>
 
 <template>
-  <h1>oooooo</h1>
+  <section ref="container" :class="$style.container" @scroll.passive="throttle(handleScroll($event), 300)">
+    <div :style="fillPadding">
+      <template v-for="data of renderData">
+        <slot :data="data"></slot>
+      </template>
+    </div>
+  </section>
 </template>
+
+<style module>
+.container {
+  width: 100%;
+  height: 100%;
+  overflow-x: hidden;
+  overflow-y: scroll;
+}
+</style>
--- src/views/VirtualScroll/index.vue
+++ src/views/VirtualScroll/index.vue
@@ -1,50 +1,27 @@
 <script setup>
-import { computed, ref } from '@vue/reactivity'
-import { nextTick } from '@vue/runtime-core'
+import { ref } from '@vue/reactivity'
 
 const totalData = ref([])
-const container = ref(null)
-const itemHeight = 80
-const containSize = ref(0)
-const startIndex = ref(0)
-const endIndex = computed(() => startIndex.value + containSize.value > totalData.value.length - 1 ? totalData.value.length - 1 : startIndex.value + containSize.value)
-const renderData = computed(() => totalData.value.slice(startIndex.value, endIndex.value))
-
-const fillPadding = computed(() => {
-  return {
-    padding: `${startIndex.value * itemHeight}px 0px ${(totalData.value.length - 1 - endIndex.value) * itemHeight}px 0px`,
-  }
-})
-
-nextTick(() => {
-  containSize.value = Math.floor(container.value.offsetHeight / itemHeight) + 2
-})
 
 fetch('https://jsonplaceholder.typicode.com/posts')
   .then((res) => {
     return res.json()
   })
   .then((data) => {
-    totalData.value = [...data, ...data, ...data]
+    totalData.value = data
   })
-
-const handleScroll = () => {
-  startIndex.value = Math.floor(container.value.scrollTop / itemHeight)
-}
 </script>
 
 <template>
   <div class="w-100 m-auto h-screen overflow-hidden">
-    <main ref="container" class="w-full h-full overflow-y-auto" @scroll.passive="handleScroll">
-      <div :style="fillPadding">
-        <div class="border-b-4 border-red-500 h-20" v-for="data of renderData">
-          <span>
-            {{ data.id }}
-            <hr />
-            {{ data.title }}
-          </span>
+    <virtual-scroll :totalData="totalData" :itemHeight="80">
+      <template v-slot:default="item">
+        <div class="h-20 border-b border-red-500 overflow-hidden">
+          {{ item.data.id }}
+          <br />
+          {{ item.data.title }}
         </div>
-      </div>
-    </main>
+      </template>
+    </virtual-scroll>
   </div>
 </template>
