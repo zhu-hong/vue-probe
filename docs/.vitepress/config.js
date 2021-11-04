9c95778dc9c1a39dc2d3099cb4e9a55fb2b39c22
zhu-hong <zhxhyy@qq.com>
2021-11-04 23:09:15 +0800

    use vitepress

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,20 @@
+export default {
+  title: 'vue-probe',
+  lang: 'zh-CN',
+  description: '我的自定义插件/组件集合,主要用于学习(更新中...)',
+  themeConfig: {
+    logo: '/logo.svg',
+    sidebar: [
+      {
+        text: '虚拟滚动',
+        link: '/guide/virtual-scroll/',
+      },
+    ],
+    nav: [
+      {
+        text: 'Github',
+        link: 'https://github.com/zhu-hong/vue-probe/',
+      },
+    ],
+  },
+}
---
+++ docs/index.md
@@ -0,0 +1,10 @@
+---
+home: true
+
+heroImage: /logo.svg
+
+actionText: 开始
+actionLink: /guide/virtual-scroll/
+
+footer: MIT Licensed | Copyright © 2021-present zhu-hong
+---
