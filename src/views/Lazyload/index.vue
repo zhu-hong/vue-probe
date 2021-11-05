691e64b7788659b94252214d640ee0c4605ea2d6
zhu-hong <zhxhyy@qq.com>
2021-11-05 10:07:26 +0800

    disorder

--- src/views/Lazyload/index.vue
+++ src/views/Lazyload/index.vue
@@ -3,9 +3,9 @@ import imgs from './data'
 </script>
 
 <template>
-<div class="h-screen overflow-y-scroll overflow-x-hidden grid grid-cols-4 gap-2 p-2 <sm:grid-cols-3">
-  <template v-for="img in imgs" :key="img.eid">
-    <img class="h-100 w-full object-cover rounded transition hover:(shadow-2xl cursor-pointer)" v-lazy="img.url" alt="AmberKuo">
-  </template>
-</div>
+  <div class="h-screen overflow-y-scroll overflow-x-hidden grid grid-cols-4 gap-2 p-2 <sm:grid-cols-3">
+    <template v-for="img in imgs" :key="img.eid">
+      <img class="h-100 w-full object-cover rounded transition hover:(shadow-2xl cursor-pointer)" v-lazy="img.url" alt="AmberKuo">
+    </template>
+  </div>
 </template>
